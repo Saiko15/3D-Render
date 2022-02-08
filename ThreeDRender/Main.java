9a081dcae03b1c2c3f42d5a6b97f5b4621f94820
Saiko15 <ahmedsaiko42@gmail.com>
2022-02-08 19:05:12 +0200

    import utilities will be used

--- ThreeDRender/Main.java
+++ ThreeDRender/Main.java
@@ -1,8 +1,16 @@
 package ThreeDRender;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        Container pane = frame.getContentPane();
+        pane.setLayout(new BorderLayout());
+
         System.out.println("Hello World");
+        // System.out.println(pane.getAlignmentX());
     }
     
 }
